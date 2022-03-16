b836d199550ea3a23f48b2b82303065661284ebf
goliveonafarm <50637542+goliveonafarm@users.noreply.github.com>
2022-03-16 16:18:34 -0400

    Add files via upload
    -added ethereal option
    -added mage plate
    -changed formatting a bit and fixed white screen load issue

--- ed.js
+++ ed.js
@@ -2,34 +2,48 @@ let inputID = null;
 let ed = null;
 let armorName = null;
 let output = null;
+let ethValue = 1.0;
+let isEthCheckBox = document.getElementById("isEthCheckbox");
+let isSup = 1;
+
+function setEth() {
+    if (isEthCheckBox.checked == true) {
+        ethValue = 1.5;
+    }
+    else {
+        ethValue = 1.0;
+    }
+    calcDef()
+}
 
 function setInputID(_inputID) {
     inputID = _inputID;
 
     armorName = document.getElementById(inputID).innerHTML;
     document.getElementById("chooseArm").innerText = armorName;
-    
+
     calcDef()
 }
 
 function setED(_ed) {
     ed = _ed;
 
-    if(ed==0){
-    document.getElementById("edTag").innerText = "Max def (non ED)";
+    if (ed == 0) {
+        document.getElementById("edTag").innerText = "Max def (non ED)";
+        isSup = 0;
     }
-    else
-    {(document.getElementById("edTag").innerText = ed + "% Enhanced Defense")};
+    else {
+        isSup = 1;
+        (document.getElementById("edTag").innerText = ed + "% Enhanced Defense")
+    }
     calcDef()
 }
 
 function calcDef() {
-    if (ed == "0") {
-        document.getElementById("outputID").innerText = "Defense: " + inputID;
-    }
-    else if (ed !== null && inputID !== null) {
-        output = Math.floor(((parseFloat(inputID) + 1) * (1 + (parseFloat(ed) * 0.01))));
-        document.getElementById("outputID").innerText = "Defense: " + output;
 
+    if (ed !== null && inputID !== null) {
+        output = Math.floor((((parseFloat(inputID) + isSup) * ethValue) * (1 + (parseFloat(ed) * 0.01))));
+        document.getElementById("outputID").innerText = "Defense: " + output;
+        
     }
-};
+}
