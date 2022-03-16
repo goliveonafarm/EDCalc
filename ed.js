b321142384f36bd48693e861c073ea743a40f652
goliveonafarm <50637542+goliveonafarm@users.noreply.github.com>
2022-03-16 14:01:28 -0400

    Add files via upload

---
+++ ed.js
@@ -0,0 +1,34 @@
+let inputID = null;
+let ed = null;
+let armorName = null;
+let output = null;
+
+function setInputID(_inputID) {
+    inputID = _inputID;
+
+    armorName = document.getElementById(inputID).innerHTML;
+    document.getElementById("chooseArm").innerText = armorName;
+    
+    calcDef()
+}
+
+function setED(_ed) {
+    ed = _ed;
+
+    if(ed==0){
+    document.getElementById("edTag").innerText = "Max def (non ED)";
+    }
+    else (document.getElementById("edTag").innerText = ed + "% Enhanced Defense");
+    calcDef()
+}
+
+function calcDef() {
+    if (ed == "0") {
+        document.getElementById("outputID").innerText = "Defense: " + inputID;
+    }
+    else if (ed !== null && inputID !== null) {
+        output = Math.floor(((parseFloat(inputID) + 1) * (1 + (parseFloat(ed) * 0.01))));
+        document.getElementById("outputID").innerText = "Defense: " + output;
+
+    }
+};
